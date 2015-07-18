cf9a039d1b3a23592d50395144192bc376cd626b
Adam Detrick <akdetrick@gmail.com>
2015-07-18 16:00:24 -0400

    gemfile and setup

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "~~~~~~~~ using correct ruby..."
+rbenv install
+
+echo "~~~~~~~~ getting latest for bundler gem..."
+gem install bundler
+
+echo "~~~~~~~~ installing required gems..."
+bundle install
+
+echo "~~~~~~~~ installing npm modules..."
+npm install
+
+echo "~~~~~~~~ installing bower components..."
+bower install
+
+echo "~~~~~~~~ rebuilding..."
+grunt
+
+exit 0
